d4c4c68081b5495fd4d693508975a85eb466e41a
Sagar Sarkale <sagarsarkale.work@gmail.com>
2026-06-12 15:58:54 +0530

    nvim: fix render-markdown — install as direct plugin instead of broken LazyVim extra

---
+++ nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,8 @@
+-- Inline markdown rendering (headings, bold, checkboxes, tables)
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {},
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
+  },
+}
