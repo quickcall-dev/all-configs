4f63edc87b43cdf6a120ac56bb3541fb0008c725
Sagar Sarkale <sagarsarkale.work@gmail.com>
2026-08-01 00:04:22 +0530

    nvim: <leader>yp yank absolute path; tmux: current dir in status-right, fix reload bind

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Yank absolute path of current file to system clipboard
+vim.keymap.set("n", "<leader>yp", function()
+  vim.fn.setreg("+", vim.fn.expand("%:p"))
+end, { desc = "Yank absolute path" })
